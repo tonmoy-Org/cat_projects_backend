b49e986f14cbbbabd9fe386e0b4c2c483fb96658
tonmoy-Org <tonmoy15-5200@diu.edu.bd>
2026-03-03 22:48:44 +0600

    added section title blog page video title

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:5173' ,
+    origin: ['http://localhost:5173', 'https://fatherofmeow.com', 'https://www.fatherofmeow.com'],
     credentials: true,
   })
 );
