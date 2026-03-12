7b73d9158f66bfa9166a3655dd4964eb536dbb26
tonmoyOrg <tonmoy15-5200@diu.edu.bd>
2026-03-12 23:04:37 +0600

    add cors url and fixed cors error

--- src/routes/carouselRoutes.js
+++ src/routes/carouselRoutes.js
@@ -12,6 +12,7 @@ const { protect } = require('../middleware/authMiddleware');
 
 const upload = multer({
   storage: multer.memoryStorage(),
+  limits: { fileSize: 5 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith('image/')) cb(null, true);
     else cb(new Error('Only image files are allowed'), false);
@@ -23,7 +24,7 @@ router.get('/', listCarousel);
 router.get('/:id', getCarousel);
 
 // Protected routes
-router.post('/', protect, createCarousel);
+router.post('/', protect, upload.single('image'), createCarousel);
 router.put('/:id', protect, upload.single('image'), updateCarousel);
 router.delete('/:id', protect, deleteCarousel);
 
